fe1eaf0650eb381632679bc45641ac6a888c9e4a
Arun Ravindran <arunvr@gmail.com>
2020-04-04 11:45:17 +0530

    Cleanup and organized constants up the loop

--- main.py
+++ main.py
@@ -22,24 +22,25 @@ def main():
     HEIGHT = 1080
     camera = Vector(0, 0, -1)
     lights = [Light(Point(1.5, -0.5, -10.0), Color.from_hex("#FF4900"))]
-    # Model of the Corona Virus
+    # Model of the Corona Virus (relative measurements from Inkscape mockup)
     radius = 0.2
-    objects = [Sphere(Point(0, 0, 0), radius, Material(Color.from_hex("#FF7701")))]
-    # Add spiky blob filaments
     spikes = 32
     stem_length = 3
+    stem_radius = 0.008
+    blob_radius = 0.01
+    # Add core
+    objects = [Sphere(Point(0, 0, 0), radius, Material(Color.from_hex("#FF7701")))]
+    # Add spiky blob filaments
     for i in range(spikes):
         angle = math.radians(360.0 / spikes * i + randint(-2, 2))
         for j in range(stem_length):
             # convert polar coordinates into Cartesian coordinates
-            stem_radius = 0.008
             x = (radius + stem_radius * j) * math.sin(angle)
             y = (radius + stem_radius * j) * math.cos(angle)
             objects.append(
                 Sphere(Point(x, y, 0), stem_radius, Material(Color.from_hex("#d47721")))
             )
         # ending blob
-        blob_radius = 0.01
         x = (radius + stem_radius * j + blob_radius) * math.sin(angle)
         y = (radius + stem_radius * j + blob_radius) * math.cos(angle)
         objects.append(
